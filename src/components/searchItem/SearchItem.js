fb35d0fb2c6608ac95f55aec0774ad9b79f0596c
Julia Tran <julia.kim.tran@hotmail.com>
2022-09-29 00:45:52 -0400

    Add details

--- src/components/searchItem/SearchItem.js
+++ src/components/searchItem/SearchItem.js
@@ -6,7 +6,7 @@ function SearchItem() {
     <div className='searchItem'>
       <img src="https://cf.bstatic.com/xdata/images/hotel/square600/261707778.webp?k=fa6b6128468ec15e81f7d076b6f2473fa3a80c255582f155cae35f9edbffdd78&o=&s=1" alt="" className="siImg" />
       <div className="siDesc">
-      <h1 className="siTitle">Tower Street Apartments</h1>
+        <h1 className="siTitle">Tower Street Apartments</h1>
         <span className="siDistance">500m from center</span>
         <span className="siTaxiOp">Free airport taxi</span>
         <span className="siSubtitle">
@@ -21,9 +21,17 @@ function SearchItem() {
         </span>
       </div>
       <div className="siDetails">
-        Details
+        <div className="siRating">
+          <span>Excellent</span>
+          <button>8.9</button>
+        </div>
+        <div className="siDetailsText">
+          <span className='siPrice'>$123 </span>
+          <span className='siTaxOp'>Includes taxes and fees </span>
+          <button className='siCheckButton'>See availabiltiy</button>
+        </div>
       </div>
-      </div>
+    </div>
   )
 }
 
