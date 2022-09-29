9bf6be45beb32e201c5a6a3190516b95710db81c
Julia Tran <julia.kim.tran@hotmail.com>
2022-09-28 21:47:38 -0400

    Add by property type

--- src/components/propertyList/PropertyList.js
+++ src/components/propertyList/PropertyList.js
@@ -6,7 +6,7 @@ function PropertyList() {
     <div className='pList'>
      <div className="pListItem">
         <img
-          src="https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-apartments_300/9f60235dc09a3ac3f0a93adbc901c61ecd1ce72e.jpg"
+          src="https://cf.bstatic.com/static/img/theme-index/carousel_320x240/bg_resorts/6f87c6143fbd51a0bb5d15ca3b9cf84211ab0884.jpg"
           alt=""
           className="pListImg"
         />
@@ -38,7 +38,7 @@ function PropertyList() {
         </div>
       </div>
       <div className="pListItem">
-        <img src="https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-apartments_300/9f60235dc09a3ac3f0a93adbc901c61ecd1ce72e.jpg" alt="" className="pListImg" />
+        <img src="https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-chalet_300/8ee014fcc493cb3334e25893a1dee8c6d36ed0ba.jpg" alt="" className="pListImg" />
         <div className="pListTitles">
           <h1>Villas </h1>
           <h2>456 Villas </h2>
