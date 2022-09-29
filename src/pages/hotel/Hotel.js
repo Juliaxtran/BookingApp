9e9654e727a49fb82f0950f29311456a631de056
Julia Tran <julia.kim.tran@hotmail.com>
2022-09-29 00:54:51 -0400

    Create hotel list item

--- src/pages/hotel/Hotel.js
+++ src/pages/hotel/Hotel.js
@@ -1,9 +1,26 @@
 import React from 'react'
-
+import './hotel.css'
+import Navbar from '../../components/navbar/Navbar'
+import Header from '../../components/header/Header'
+import FmdGoodIcon from '@mui/icons-material/FmdGood';
 
 function Hotel() {
   return (
-    <div>Hotel</div>
+    <div>
+      <Navbar />
+      <Header type='list'/>
+      <div className="hotelContainer">
+        <div className="hotelWrapper">
+         <h1 className='hotelTitle'> Grand Hotel</h1>
+         <div className="hotelAddress">
+        <FmdGoodIcon className='hotelIcon'/>
+        <span>12948 Bukingham palace</span>
+         </div>
+         <span className='hotelDistance'>Excellent location - 500m from center</span>
+         <span className='hotelPriceHighlight'>Book a stay over $114 at this property and get a free airport taxi</span>
+        </div>
+      </div>
+    </div>
   )
 }
 
