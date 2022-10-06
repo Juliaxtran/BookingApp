b153b416b13e3da74be0f29fb1d0df0164da4c0a
Julia Tran <julia.kim.tran@hotmail.com>
2022-10-06 17:20:14 -0400

    Fetch data from countByCity route

--- src/components/featured/Featured.js
+++ src/components/featured/Featured.js
@@ -1,6 +1,14 @@
+import useFetch from "../../hooks/useFetch";
 import "./featured.css";
 
 const Featured = () => {
+
+  const url = "http://localhost:3002/api/hotels/countByCity?cities=berlin,madrid,london";
+  
+  const {data, loading, error} = useFetch(url);
+
+  console.log("DATA", data);
+
   return (
     <div className="featured">
       <div className="featuredItem">
