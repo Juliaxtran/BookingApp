23db8d73b8e8335b87636ff97360c18c3def5395
Julia Tran <julia.kim.tran@hotmail.com>
2022-10-11 17:25:02 -0400

    Count by type

--- src/components/propertyList/PropertyList.js
+++ src/components/propertyList/PropertyList.js
@@ -1,58 +1,40 @@
-import React from 'react'
-import './propertyList.css'
+import useFetch from "../../hooks/useFetch";
+import "./propertyList.css";
 
-function PropertyList() {
+const PropertyList = () => {
+  const { data, loading, error } = useFetch("http://localhost:3002/api/hotels/countByType");
+
+  const images = [
+    "https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-chalet_300/8ee014fcc493cb3334e25893a1dee8c6d36ed0ba.jpg",
+    "https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-apartments_300/9f60235dc09a3ac3f0a93adbc901c61ecd1ce72e.jpg",
+    "https://cf.bstatic.com/static/img/theme-index/carousel_320x240/bg_resorts/6f87c6143fbd51a0bb5d15ca3b9cf84211ab0884.jpg",
+    "https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-apartments_300/9f60235dc09a3ac3f0a93adbc901c61ecd1ce72e.jpg",
+        "https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-chalet_300/8ee014fcc493cb3334e25893a1dee8c6d36ed0ba.jpg",
+  ];
   return (
-    <div className='pList'>
-     <div className="pListItem">
-        <img
-          src="https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-apartments_300/9f60235dc09a3ac3f0a93adbc901c61ecd1ce72e.jpg"
-          alt=""
-          className="pListImg"
-        />
-        <div className="pListTitles">
-          <h1>Hotels</h1>
-          <h2>233 hotels</h2>
-        </div>
-      </div>
-      <div className="pListItem">
-        <img
-          src="https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-apartments_300/9f60235dc09a3ac3f0a93adbc901c61ecd1ce72e.jpg"
-          alt=""
-          className="pListImg"
-        />
-        <div className="pListTitles">
-          <h1>Apartments</h1>
-          <h2>2331 hotels</h2>
-        </div>
-      </div>
-      <div className="pListItem">
-        <img
-          src="https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-chalet_300/8ee014fcc493cb3334e25893a1dee8c6d36ed0ba.jpg"
-          alt=""
-          className="pListImg"
-        />
-        <div className="pListTitles">
-          <h1>Resorts</h1>
-          <h2>2331 hotels</h2>
-        </div>
-      </div>
-      <div className="pListItem">
-        <img src="https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-chalet_300/8ee014fcc493cb3334e25893a1dee8c6d36ed0ba.jpg" alt="" className="pListImg" />
-        <div className="pListTitles">
-          <h1>Villas </h1>
-          <h2>456 Villas </h2>
-        </div>
-      </div>
-      <div className="pListItem">
-        <img src="https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-chalet_300/8ee014fcc493cb3334e25893a1dee8c6d36ed0ba.jpg" alt="" className="pListImg" />
-        <div className="pListTitles">
-          <h1>Cabins </h1>
-          <h2>1295 Cabins</h2>
-        </div>
-      </div>
+    <div className="pList">
+      {loading ? (
+        "loading"
+      ) : (
+        <>
+          {data &&
+            images.map((img,i) => (
+              <div className="pListItem" key={i}>
+                <img
+                  src={img}
+                  alt=""
+                  className="pListImg"
+                />
+                <div className="pListTitles">
+                  <h1>{data[i].type}</h1>
+                  <h2>{data[i].count} {data[i].type}</h2>
+                </div>
+              </div>
+            ))}
+        </>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default PropertyList
+export default PropertyList;
