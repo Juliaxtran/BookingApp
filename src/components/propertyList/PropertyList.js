a0e0028e5a1a091db514d0862f48ee3bdde13573
Julia Tran <julia.kim.tran@hotmail.com>
2022-10-11 17:31:39 -0400

    Capitalize property type

--- src/components/propertyList/PropertyList.js
+++ src/components/propertyList/PropertyList.js
@@ -2,7 +2,9 @@ import useFetch from "../../hooks/useFetch";
 import "./propertyList.css";
 
 const PropertyList = () => {
-  const { data, loading, error } = useFetch("http://localhost:3002/api/hotels/countByType");
+
+  const url = "http://localhost:3002/api/hotels/countByType";
+  const { data, loading, error } = useFetch(url);
 
   const images = [
     "https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-chalet_300/8ee014fcc493cb3334e25893a1dee8c6d36ed0ba.jpg",
@@ -11,6 +13,10 @@ const PropertyList = () => {
     "https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-apartments_300/9f60235dc09a3ac3f0a93adbc901c61ecd1ce72e.jpg",
         "https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-chalet_300/8ee014fcc493cb3334e25893a1dee8c6d36ed0ba.jpg",
   ];
+
+  if (error ) {
+    console.log(error)
+  }
   return (
     <div className="pList">
       {loading ? (
@@ -26,8 +32,8 @@ const PropertyList = () => {
                   className="pListImg"
                 />
                 <div className="pListTitles">
-                  <h1>{data[i].type}</h1>
-                  <h2>{data[i].count} {data[i].type}</h2>
+                  <h1>{data[i]?.type}</h1>
+                  <h2>{data[i]?.count} {data[i]?.type}</h2>
                 </div>
               </div>
             ))}
@@ -36,5 +42,4 @@ const PropertyList = () => {
     </div>
   );
 };
-
 export default PropertyList;
